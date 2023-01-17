04a12552c7760f3c507dbddb706b95e7544b72b5
Anshu <anshukumarsingh448@gmail.com>
2023-01-17 16:12:06 +0530

    sos app

--- app/src/main/java/com/darkness/sparkwomen/ContactActivity.kt
+++ app/src/main/java/com/darkness/sparkwomen/ContactActivity.kt
@@ -1,190 +1,128 @@
-package com.darkness.sparkwomen;
+package com.darkness.sparkwomen
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.darkness.sparkwomen.ContactsAdapter
+import com.darkness.sparkwomen.MyOnClickListener
+import android.content.Intent
+import com.darkness.sparkwomen.MainActivity
+import android.os.Bundle
+import com.darkness.sparkwomen.R
+import android.content.SharedPreferences
+import android.view.View
+import android.view.Window
+import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import java.util.ArrayList
+import java.util.HashMap
+import java.util.LinkedHashSet
 
-import android.app.Dialog;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.Set;
-
-public class ContactActivity extends AppCompatActivity {
-
-
-    EditText contact;
-    Button addContact;
-
-    RecyclerView recyclerView;
-
-    HashMap<String,String> contacts;
-
-    ArrayList<String> send;
-
-    ContactsAdapter adapter;
-    MyOnClickListener onClickListener;
-
-    ImageView edit;
-
-    TextView callerInfo;
-
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        startActivity(new Intent(this,MainActivity.class));
-        this.finish();
+class ContactActivity : AppCompatActivity() {
+    private lateinit var contact: EditText
+    private lateinit var addContact: Button
+   private lateinit var recyclerView: RecyclerView
+    var contacts: HashMap<String, String>? = null
+    var send: ArrayList<String>? = null
+    var adapter: ContactsAdapter? = null
+    var onClickListener: MyOnClickListener? = null
+   private lateinit var edit: ImageView
+    var callerInfo: TextView? = null
+    override fun onBackPressed() {
+        super.onBackPressed()
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact);
-
-
-
-        edit = findViewById(R.id.editCallButton);
-
-
-        edit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Dialog dialog = new Dialog(ContactActivity.this);
-                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                dialog.setCancelable(false);
-                dialog.setContentView(R.layout.dialog);
-
-                Button close,save;
-                close = dialog.findViewById(R.id.dialogCancel);
-                save = dialog.findViewById(R.id.dialogSave);
-                EditText  number = dialog.findViewById(R.id.dialogEditText);
-
-                save.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        String numberText = number.getText().toString();
-                        if(numberText.length() == 10){
-                            SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
-                            SharedPreferences.Editor editor = sharedPreferences.edit();
-                            editor.putString("firstNumber",numberText);
-                            editor.apply();
-                            setCallingInformation();
-                            dialog.dismiss();
-                        }else {
-                            Toast.makeText(ContactActivity.this, "Enter valid number!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
-                close.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        dialog.dismiss();
-                    }
-                });
-
-
-                dialog.show();
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+        edit = findViewById(R.id.editCallButton)
+        edit.setOnClickListener(View.OnClickListener {
+            val dialog = Dialog(this@ContactActivity)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCancelable(false)
+            dialog.setContentView(R.layout.dialog)
+            val close: Button
+            val save: Button
+            close = dialog.findViewById(R.id.dialogCancel)
+            save = dialog.findViewById(R.id.dialogSave)
+            val number = dialog.findViewById<EditText>(R.id.dialogEditText)
+            save.setOnClickListener {
+                val numberText = number.text.toString()
+                if (numberText.length == 10) {
+                    val sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
+                    val editor = sharedPreferences.edit()
+                    editor.putString("firstNumber", numberText)
+                    editor.apply()
+                    setCallingInformation()
+                    dialog.dismiss()
+                } else {
+                    Toast.makeText(this@ContactActivity, "Enter valid number!", Toast.LENGTH_SHORT)
+                        .show()
+                }
             }
-        });
-
-
-
-
-        callerInfo = findViewById(R.id.callText);
-
-
-        setCallingInformation();
-
-
-        contacts = new HashMap<>();
-        send = new ArrayList<>();
-
-        adapter = new ContactsAdapter(this, send, new MyOnClickListener() {
-            @Override
-            public void onItemClicked(int position) {
-                deleteItemFromDatabase(position);
-            }
-        });
-
-        recyclerView = findViewById(R.id.contacts);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        getData();
-
-        contact = findViewById(R.id.contactGet);
-        addContact = findViewById(R.id.addContact);
-
-        addContact.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                createContact(contact.getText().toString());
-            }
-        });
+            close.setOnClickListener { dialog.dismiss() }
+            dialog.show()
+        })
+        callerInfo = findViewById(R.id.callText)
+        setCallingInformation()
+        contacts = HashMap()
+        send = ArrayList()
+        adapter = ContactsAdapter(this, send) { position -> deleteItemFromDatabase(position) }
+        recyclerView = findViewById(R.id.contacts)
+        recyclerView.setAdapter(adapter)
+        recyclerView.setLayoutManager(LinearLayoutManager(this))
+        data
+        contact = findViewById(R.id.contactGet)
+        addContact = findViewById(R.id.addContact)
+        addContact.setOnClickListener(View.OnClickListener {
+            createContact(
+                contact.getText().toString()
+            )
+        })
     }
 
-    private void createContact(String contactString) {
-        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        Set<String> oldNumbers = sharedPreferences.getStringSet("enumbers", new LinkedHashSet<>());
-        oldNumbers.add(contactString);
-        editor.remove("enumbers");
-        editor.putStringSet("enumbers",oldNumbers);
-        editor.apply();
-
-        contact.setText("");
-        editor.apply();
-        getData();
-
+    private fun createContact(contactString: String) {
+        val sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val oldNumbers = sharedPreferences.getStringSet("enumbers", LinkedHashSet())
+        oldNumbers!!.add(contactString)
+        editor.remove("enumbers")
+        editor.putStringSet("enumbers", oldNumbers)
+        editor.apply()
+        contact!!.setText("")
+        editor.apply()
+        data
     }
 
-
-    private void setCallingInformation(){
-        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
-        String firstNumber = sharedPreferences.getString("firstNumber","null");
-
-        if (firstNumber.isEmpty()||firstNumber.equalsIgnoreCase("null")){
-            callerInfo.setText("Please add number.");
-        }else {
-            callerInfo.setText(firstNumber);
+    private fun setCallingInformation() {
+        val sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
+        val firstNumber = sharedPreferences.getString("firstNumber", "null")
+        if (firstNumber!!.isEmpty() || firstNumber.equals("null", ignoreCase = true)) {
+            callerInfo!!.text = "Please add number."
+        } else {
+            callerInfo!!.text = firstNumber
         }
-
     }
 
-
-
-
-    private void deleteItemFromDatabase(int position) {
-        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
-        Set<String> oldNumbers = sharedPreferences.getStringSet("enumbers", new LinkedHashSet<>());
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.remove("enumbers");
-        oldNumbers.remove(send.get(position));
-        editor.putStringSet("enumbers",oldNumbers);
-        editor.apply();
-        getData();
+    private fun deleteItemFromDatabase(position: Int) {
+        val sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
+        val oldNumbers = sharedPreferences.getStringSet("enumbers", LinkedHashSet())
+        val editor = sharedPreferences.edit()
+        editor.remove("enumbers")
+        oldNumbers!!.remove(send!![position])
+        editor.putStringSet("enumbers", oldNumbers)
+        editor.apply()
+        data
     }
 
-
-
-    private void getData() {
-        send.clear();
-        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
-        Set<String> oldNumbers = sharedPreferences.getStringSet("enumbers", new LinkedHashSet<>());
-        send.addAll(oldNumbers);
-        adapter.notifyDataSetChanged();
-    }
+    private val data: Unit
+        private get() {
+            send!!.clear()
+            val sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
+            val oldNumbers = sharedPreferences.getStringSet("enumbers", LinkedHashSet())
+            send!!.addAll(oldNumbers!!)
+            adapter!!.notifyDataSetChanged()
+        }
 }
